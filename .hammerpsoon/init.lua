58bf0e9ea16c7fe840d93cfd62309b375d820fed
Bryce Mecum <petridish@gmail.com>
2024-11-06 11:26:26 -0800

    Update init.lua

--- .hammerpsoon/init.lua
+++ .hammerpsoon/init.lua
@@ -1,16 +1,22 @@
-local hyper = {"cmd", "alt", "ctrl", "shift"}
+-- VARIABLES
+local hyper = { "cmd", "alt", "ctrl", "shift" }
+
+-- FUNCTIONS
+-- TODO
+
+-- KEYBINDS
 
 -- Move window to left half of screen
 hs.hotkey.bind(hyper, "I", function()
   local win = hs.window.focusedWindow()
-  if not win then return end  -- If no window is focused, do nothing
+  if not win then return end -- If no window is focused, do nothing
 
   local screen = win:screen()
   local screenFrame = screen:frame()
 
   -- Calculate new size (2/3 of screen dimensions)
-  local newWidth = screenFrame.w * (2/3)
-  local newHeight = screenFrame.h * (2/3)
+  local newWidth = screenFrame.w * (2 / 3)
+  local newHeight = screenFrame.h * (2 / 3)
 
   -- Calculate new position
   local newX = screenFrame.x + (screenFrame.w - newWidth) / 2
@@ -18,9 +24,55 @@ hs.hotkey.bind(hyper, "I", function()
 
   -- Set new frame
   win:setFrame({
-      x = newX,
-      y = newY,
-      w = newWidth,
-      h = newHeight
+    x = newX,
+    y = newY,
+    w = newWidth,
+    h = newHeight
   })
 end)
+
+-- Reload Config
+hs.hotkey.bind(hyper, "r", function()
+  hs.reload()
+end)
+
+-- MOUSEBINDS
+local mouseButton3 = 3
+local mouseButton4 = 4
+
+-- Extra mouse buttons
+hs.eventtap.new({ hs.eventtap.event.types.otherMouseDown }, function(event)
+  -- Get modifier keys and button
+  local flags = event:getFlags()
+  local button = event:getProperty(hs.eventtap.event.properties.mouseEventButtonNumber)
+
+
+  -- Mouse3
+  if not flags.alt and button == mouseButton3 then
+    hs.eventtap.keyStroke({ "cmd", "shift" }, "[", 0)
+    return true
+  end
+
+  -- Mouse4
+  if not flags.alt and button == mouseButton4 then
+    hs.eventtap.keyStroke({ "cmd", "shift" }, "]", 0)
+    return true
+  end
+
+  -- Option+Mouse3
+  if flags.alt and button == mouseButton3 then
+    hs.eventtap.keyStroke({ "cmd" }, "[", 0)
+    return true
+  end
+
+  -- Option+Mouse4
+  if flags.alt and button == mouseButton4 then
+    hs.eventtap.keyStroke({ "cmd" }, "]", 0)
+    return true
+  end
+
+  return false
+end):start()
+
+-- always announce on reload
+hs.alert.show("Config Loaded")
