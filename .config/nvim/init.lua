83c598a0c5ae2d06d641f37811e3a92f3005086b
Bryce Mecum <petridish@gmail.com>
2025-09-13 13:21:18 -0700

    Sync nvim setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -40,6 +40,26 @@ plugins = {
 	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' }
+	},
+	{
+	  "folke/flash.nvim",
+	  event = "VeryLazy",
+	  ---@type Flash.Config
+	  opts = {},
+	  -- stylua: ignore
+	  keys = {
+	    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+	    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+	    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+	    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+	    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+	  },
+	},
+	{
+	   "m4xshen/hardtime.nvim",
+	   lazy = false,
+	   dependencies = { "MunifTanjim/nui.nvim" },
+	   opts = {},
 	}
 }
 
@@ -58,3 +78,6 @@ vim.cmd[[colorscheme tokyonight]]
 
 --- lualine
 require('lualine').setup()
+--- hardtime
+require("hardtime").setup()
+
