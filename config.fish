d89fedf114a26a977698d9863612fe9c32fcac14
Bryce Mecum <petridish@gmail.com>
2025-11-18 10:34:50 -0800

    fish: merge work machine into config

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
-# Homebrew
+# brew
 set current_arch (uname -m)
 
 if test $current_arch = "arm64"
@@ -9,22 +9,34 @@ else
     echo "Archtecture unknown, failed to initialize Homebrew."
 end
 
-# ENV
+# prompt
+set -U pure_symbol_prompt "\$"
+
+# env vars
 set -gx HOMEBREW_NO_ENV_HINTS 0
 set -gx MAKEFLAGS -j8
 set -gx EDITOR vim
 
-# PATH
-fish_add_path ~/bin
-fish_add_path ~/go/bin
-fish_add_path ~/.cargo/bin
+# paths
+fish_add_path "/opt/homebrew/bin"
+fish_add_path "$HOME/.cargo/bin"
+fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/go/bin"
+fish_add_path "$HOME/.deno/bin"
 
 # aliases
 alias R="R --no-save --no-restore-data --quiet"
 alias be="bundle exec"
 alias k="kubectl"
 alias rstudio="open -na Rstudio"
+# aliases: git
+alias gc="git commit"
+alias gap="git add -p"
 
 # arrow
 set -gx ARROW_HOME "$HOME/builds/arrow-$(arch)"
 set -gx ARROW_R_DEV "TRUE"
+set -gx ADBC_HOME "$HOME/builds/adbc-$(arch)"
+
+# 1p
+source "$HOME/.config/op/plugins.sh"
