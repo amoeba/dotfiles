7f0b535402e8c7af6dac6094ee56959aed541f09
Bryce Mecum <petridish@gmail.com>
2026-03-22 09:56:53 -0700

    update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,13 +1,17 @@
-
+;; package
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+;; use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
 ;;; ui
 (tool-bar-mode -1)
 (setq inhibit-startup-screen t)
-
-;;; font
 (set-face-attribute 'default nil :font "JetBrains Mono-13")
 
 ;;; projectile
@@ -19,16 +23,33 @@
   :config
   (setq projectile-project-search-path '("~/src/"))
   (setq projectile-project-search-depth 3))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(projectile vertico pulsar orderless multiple-cursors expand-region consult)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;; vertico
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode 1))
+
+;; corfu
+(use-package corfu
+  :ensure t
+  :init
+  (global-corfu-mode 1))
+
+;; marginalia
+(use-package marginalia
+  :ensure t
+  :init
+  (marginalia-mode 1))
+
+;; which-key
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+;; magit
+(use-package magit
+  :ensure t)
+
+tin
